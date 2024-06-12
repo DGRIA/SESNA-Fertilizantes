c0413802485224c4d5664f1fd2ff4a1527b38e2f
Bvallegc <116285501+Bvallegc@users.noreply.github.com>
2024-06-12 12:53:23 +0200

    toques finales, cambio de data_cleaning_and_merge para añadir data cleaning de beneficiarios + cambios en la demo

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ import pandas as pd
 import base64
 from io import StringIO
 from src.dataset_download import download_datasets
-from src.data_cleaning_and_merge import data_cleaning
+from src.data_cleaning_and_merge import data_cleaning, data_cleaning2
 from src.scrape_urls import scrape_urls
+from streamlit_option_menu import option_menu as om
 # Incluir estas líneas en cada script para registrar los logs
 logger = logging.getLogger("Fertilizantes")
 logger.setLevel(logging.INFO)
@@ -27,9 +28,16 @@ def clear_directory(directory):
         except Exception as e:
             print(f'Failed to delete {file_path}. Reason: {e}')
 
+def data_cleaning_function(dataset):
+    if dataset == 'productores_autorizados_2023':
+        data_cleaning()
+        st.success("El proceso de limpieza de datos ha terminado. En la siguiente pestaña puede proceder con la descarga de los datos estandarizados.")
+    elif dataset == 'beneficiarios_fertilizantes_2023':
+        data_cleaning2()
+        st.success("El proceso de limpieza de datos ha terminado. En la siguiente pestaña puede proceder con la descarga de los datos estandarizados.")
 
 def show_intro():
-    if st.session_state.main_page == 'Productores autorizados':
+    if st.session_state.main_page == 'Productores autorizados 2023':
         st.markdown((
         """
             La siguiente aplicacion ha sido desarrollada para [SESNA](https://www.sesna.gob.mx/).
@@ -67,7 +75,7 @@ def show_intro():
             # Reset the StringIO object to the beginning
             stringio.seek(0)
             dataframe = pd.read_csv(stringio)
-            dataframe.to_csv('data/' + uploaded_file.name)
+            dataframe.to_csv(os.path.join('data', uploaded_file.name))
         else:
             st.write("Uploaded file is not a CSV file.")
 
@@ -78,62 +86,47 @@ def show_intro():
     inner_cols[2].image('docs/images/mottum2.png', use_column_width=True) 
 
 def start_process():
-    cols_button = st.columns([1, 3, 1])
-    if cols_button[1].button('Pulsa para comenzar el proceso de descarga y limpieza de datos.', key='start_process_button'):
-        data_cleaning_and_merge()
-
-    st.markdown("<br>", unsafe_allow_html=True)
-    cols = st.columns([1, 1, 1])
-    inner_cols = cols[2].columns([1, 1, 1, 1])
-    inner_cols[0].markdown("<p style='text-align: center; font-family: Comic Sans MS; padding-top: 12px; white-space: nowrap;'>Made with love</p>", unsafe_allow_html=True)
-    inner_cols[2].image('docs/images/mottum2.png', use_column_width=True)
-
-def show_finished():
-    if os.path.exists("data/listado_productores_complete2023.csv"):
-        st.markdown("<h2 style='text-align: center;'>¡El dataset está listo!</h2>", unsafe_allow_html=True)
-        cols = st.columns([1, 2, 1])
-        with open("data/listado_productores_complete2023.csv", "rb") as file:
-            button_clicked = cols[1].download_button(
-                label="Pulsa aquí para descargar el dataset completo.",
-                data=file,
-                file_name="merged_dataset.csv",
-                mime="text/csv",
-            )
+    if st.session_state.main_page == 'Productores autorizados 2023':
+        cols_button = st.columns([1, 3, 1])
+        if cols_button[1].button('Descargar Listado Autorizados2023.', key='start_process_button'):
+            data_download("https://www.datos.gob.mx/busca/dataset/programa-de-fertilizantes-2023-listados-autorizados",  "data/productores_autorizados")
         st.markdown("<br>", unsafe_allow_html=True)
-        cols = st.columns([1, 1, 1])  # Create three columns
-        inner_cols = cols[2].columns([1, 1, 1, 1])  # Create two columns inside the middle column
-        inner_cols[0].markdown("<p style='text-align: center; font-family: Comic Sans MS; padding-top: 12px; white-space: nowrap;'>Made with love</p>", unsafe_allow_html=True) # Center the text, change the font, and add padding
-        inner_cols[2].image('docs/images/mottum2.png', use_column_width=True)  # Colocar la im
-
-    else:
-        st.markdown(
-            "<h2 style='text-align: center;'>Necesitas ejecutar el proceso antes de venir a esta pantalla.</h2>",
-            unsafe_allow_html=True)
+        cols = st.columns([1, 1, 1])
+        inner_cols = cols[2].columns([1, 1, 1, 1])
+        inner_cols[0].markdown("<p style='text-align: center; font-family: Comic Sans MS; padding-top: 12px; white-space: nowrap;'>Made with love</p>", unsafe_allow_html=True)
+        inner_cols[2].image('docs/images/mottum2.png', use_column_width=True)
+    elif st.session_state.main_page == 'Beneficiarios fertilizantes 2023':
+        cols_button = st.columns([1, 3, 1])
+        if cols_button[1].button('Descargar Listado Beneficiarios2023.', key='start_process_button'):
+            data_download("https://www.datos.gob.mx/busca/dataset/programa-de-fertilizantes-2023-listados-de-beneficiarios",  "data/productores_beneficiarios")
         st.markdown("<br>", unsafe_allow_html=True)
+        cols = st.columns([1, 1, 1])
+        inner_cols = cols[2].columns([1, 1, 1, 1])
+        inner_cols[0].markdown("<p style='text-align: center; font-family: Comic Sans MS; padding-top: 12px; white-space: nowrap;'>Made with love</p>", unsafe_allow_html=True)
+        inner_cols[2].image('docs/images/mottum2.png', use_column_width=True)
 
-        cols = st.columns([1, 1, 1])  # Crear tres columnas
-        cols[1].image('docs/images/mottum.svg', use_column_width=True)  # Colocar la im
-
-def data_cleaning_and_merge():
+def data_download(url, download_destination_folder):
     if not os.path.exists('data'):
             os.makedirs('data')
             print("Directory 'data' missing, creating data directory.")
     if not os.path.exists('data/productores_autorizados'):
         os.makedirs('data/productores_autorizados')
         print("Directory 'data/productores_autorizados' missing, creating data/productores_autorizados.")
+    if not os.path.exists('data/productores_beneficiarios'):
+        os.makedirs('data/productores_autorizados')
+        print("Directory 'data/productores_autorizados' missing, creating data/productores_autorizados.")
 
     clear_directory('data/productores_autorizados')
+    clear_directory('data/productores_beneficiarios')
     with st.spinner(
             'Ejecutando scripts... Esto puede tardar unos minutos. No cambie de pestaña hasta que el proceso haya acabado!'
         ):
         progress_bar = st.progress(0)
         download_urls = []
-        url = "https://www.datos.gob.mx/busca/dataset/programa-de-fertilizantes-2023-listados-autorizados"
         urls = scrape_urls(url)
         progress_bar.progress(0.33)
         for url in urls:
             download_urls.append(url)
-        download_destination_folder = "data/productores_autorizados"
         result = download_datasets(download_urls, download_destination_folder)
         progress_bar.progress(0.66)
 
@@ -171,13 +164,87 @@ def data_cleaning_and_merge():
         # Create a download button for the CSV file
         st.markdown(href, unsafe_allow_html=True)
 
-        data_cleaning()
-
         progress_bar.progress(1)  # Update progress bar to 100%
         logger.info("Fin de Ejecución")
         st.success(
-            "El proceso de descarga ha terminado. Ahora procederemos con la transformación y carga de los datos."
+            "El proceso de descarga ha terminado. En la siguiente pestaña puede proceder con la limpieza de los datos."
         )
+
+    st.markdown("<br>", unsafe_allow_html=True)
+    cols = st.columns([1, 1, 1])  # Create three columns
+    inner_cols = cols[2].columns([1, 1, 1, 1])  # Create two columns inside the middle column
+    inner_cols[0].markdown("<p style='text-align: center; font-family: Comic Sans MS; padding-top: 12px; white-space: nowrap;'>Made with love</p>", unsafe_allow_html=True) # Center the text, change the font, and add padding
+    inner_cols[2].image('docs/images/mottum2.png', use_column_width=True)
+
+def clean_data_screen():
+    if st.session_state.main_page == 'Productores autorizados 2023':
+        with st.spinner(
+            'Ejecutando scripts... Esto puede tardar unos minutos. No cambie de pestaña hasta que el proceso haya acabado!'
+        ):
+            cols_button = st.columns([1, 3, 1])
+            if cols_button[1].button('Limpieza de datos de Listado Productores2023.', key='start_process_button'):
+                data_cleaning_function('productores_autorizados_2023')
+            st.markdown("<br>", unsafe_allow_html=True)
+            cols = st.columns([1, 1, 1])
+            inner_cols = cols[2].columns([1, 1, 1, 1])
+            inner_cols[0].markdown("<p style='text-align: center; font-family: Comic Sans MS; padding-top: 12px; white-space: nowrap;'>Made with love</p>", unsafe_allow_html=True)
+            inner_cols[2].image('docs/images/mottum2.png', use_column_width=True)
+    elif st.session_state.main_page == 'Beneficiarios fertilizantes 2023':
+        with st.spinner(
+            'Ejecutando scripts... Esto puede tardar unos minutos. No cambie de pestaña hasta que el proceso haya acabado!'
+        ):
+            cols_button = st.columns([1, 3, 1])
+            if cols_button[1].button('Limpieza de datos de Listado Beneficiarios2023.', key='start_process_button'):
+                data_cleaning_function('beneficiarios_fertilizantes_2023')
+            st.markdown("<br>", unsafe_allow_html=True)
+            cols = st.columns([1, 1, 1])
+            inner_cols = cols[2].columns([1, 1, 1, 1])
+            inner_cols[0].markdown("<p style='text-align: center; font-family: Comic Sans MS; padding-top: 12px; white-space: nowrap;'>Made with love</p>", unsafe_allow_html=True)
+            inner_cols[2].image('docs/images/mottum2.png', use_column_width=True)
+
+def show_finished():
+    if st.session_state.main_page == 'Productores autorizados 2023':
+        if os.path.exists("data/listado_productores_complete2023.csv"):
+            st.markdown("<h2 style='text-align: center;'>¡El dataset está listo!</h2>", unsafe_allow_html=True)
+            cols = st.columns([1, 2, 1])
+            with open("data/listado_productores_complete2023.csv", "rb") as file:
+                button_clicked = cols[1].download_button(
+                    label="Pulsa aquí para descargar el dataset completo.",
+                    data=file,
+                    file_name="listado_productores_completo2023.csv",
+                    mime="text/csv",
+                )
+            st.markdown("<br>", unsafe_allow_html=True)
+            cols = st.columns([1, 1, 1])  # Create three columns
+            inner_cols = cols[2].columns([1, 1, 1, 1])  # Create two columns inside the middle column
+            inner_cols[0].markdown("<p style='text-align: center; font-family: Comic Sans MS; padding-top: 12px; white-space: nowrap;'>Made with love</p>", unsafe_allow_html=True) # Center the text, change the font, and add padding
+            inner_cols[2].image('docs/images/mottum2.png', use_column_width=True)
+    elif st.session_state.main_page == 'Beneficiarios fertilizantes 2023':
+        if os.path.exists("data/LISTADO_BENEFICIARIOS2023_COMPLETO.csv"):
+            st.markdown("<h2 style='text-align: center;'>¡El dataset está listo!</h2>", unsafe_allow_html=True)
+            cols = st.columns([1, 2, 1])
+            with open("data/LISTADO_BENEFICIARIOS2023_COMPLETO.csv", "rb") as file:
+                button_clicked = cols[1].download_button(
+                    label="Pulsa aquí para descargar el dataset completo.",
+                    data=file,
+                    file_name="LISTADO_BENEFICIARIOS2023_COMPLETO.csv",
+                    mime="text/csv",
+                )
+            st.markdown("<br>", unsafe_allow_html=True)
+            cols = st.columns([1, 1, 1])  # Create three columns
+            inner_cols = cols[2].columns([1, 1, 1, 1])  # Create two columns inside the middle column
+            inner_cols[0].markdown("<p style='text-align: center; font-family: Comic Sans MS; padding-top: 12px; white-space: nowrap;'>Made with love</p>", unsafe_allow_html=True) # Center the text, change the font, and add padding
+            inner_cols[2].image('docs/images/mottum2.png', use_column_width=True)
+
+    else:
+        st.markdown(
+            "<h2 style='text-align: center;'>Necesitas ejecutar el proceso antes de venir a esta pantalla.</h2>",
+            unsafe_allow_html=True)
+        st.markdown("<br>", unsafe_allow_html=True)
+
+        cols = st.columns([1, 1, 1])  # Crear tres columnas
+        cols[1].image('docs/images/mottum.svg', use_column_width=True)  # Colocar la im
+
 
 if __name__ == '__main__':
     st.markdown('''
@@ -190,6 +257,37 @@ if __name__ == '__main__':
     st.markdown("<br>", unsafe_allow_html=True)
     # Sidebar
     st.sidebar.image('docs/images/SESNA-logo.png', use_column_width=True)
+
+    st.sidebar.markdown("<br>", unsafe_allow_html=True)
+
+    if 'main_page' not in st.session_state:
+        st.session_state.main_page = 'Productores autorizados 2023'
+    with st.sidebar:
+        st.session_state.main_page = om('Main menu', ['Productores autorizados 2023', 'Beneficiarios fertilizantes 2023'], icons=["list-task", "list-task"], menu_icon="cast")
+    if st.session_state.main_page == 'Productores autorizados 2023':
+        if 'sub_page' not in st.session_state:
+            st.session_state.sub_page = '1. Introducción'
+        st.session_state.sub_page = st.radio('Productores autorizados 2023', ['1. Introducción', '2. Descarga y Transformación', '3. Limpieza de datos', '4. Descarga de los datos estandarizados'])
+        if st.session_state.sub_page == '1. Introducción':
+            show_intro()
+        elif st.session_state.sub_page == '2. Descarga y Transformación':
+            start_process()
+        elif st.session_state.sub_page == '3. Limpieza de datos':
+            clean_data_screen()
+        elif st.session_state.sub_page == '4. Descarga de los datos estandarizados':
+            show_finished()
+    elif st.session_state.main_page == 'Beneficiarios fertilizantes 2023':
+        if 'second_sub_page' not in st.session_state:
+            st.session_state.second_sub_page = '1. Introducción'
+        st.session_state.second_sub_page = st.radio('Beneficiarios fertilizantes 2023', ['1. Introducción', '2. Descarga y Transformación', '3. Limpieza de datos', '4. Descarga de los datos estandarizados'])
+        if st.session_state.second_sub_page == '1. Introducción':
+            show_intro()
+        elif st.session_state.second_sub_page == '2. Descarga y Transformación':
+            start_process()
+        elif st.session_state.second_sub_page == '3. Limpieza de datos':
+            clean_data_screen()
+        elif st.session_state.second_sub_page == '4. Descarga de los datos estandarizados':
+            show_finished()
 
     st.sidebar.markdown(
         """
@@ -208,30 +306,7 @@ if __name__ == '__main__':
         para el bienestar.
         """
     )
-
-    st.sidebar.markdown("<br>", unsafe_allow_html=True)
-
     # Initialize session state variables
-    if 'main_page' not in st.session_state:
-        st.session_state.main_page = 'Productores autorizados'
-    st.session_state.main_page = st.sidebar.radio('Navegación', ['Productores autorizados', 'Beneficiarios fertilizantes 2023'])
-    if st.session_state.main_page == 'Productores autorizados':
-        if 'sub_page' not in st.session_state:
-            st.session_state.sub_page = '1. Introducción'
-        st.session_state.sub_page = st.radio('Productores autorizados', ['1. Introducción', '2. Descarga y Transformación', '3. Descarga de los datos estandarizados'])
-        if st.session_state.sub_page == '1. Introducción':
-            show_intro()
-        elif st.session_state.sub_page == '2. Descarga y Transformación':
-            start_process()
-        elif st.session_state.sub_page == '3. Descarga de los datos estandarizados':
-            show_finished()
-    elif st.session_state.main_page == 'Beneficiarios fertilizantes 2023':
-        if 'second_sub_page' not in st.session_state:
-            st.session_state.second_sub_page = '1. Introducción'
-        st.session_state.second_sub_page = st.radio('Beneficiarios fertilizantes 2023', ['1. Introducción', '2. Proceso'])
-        if st.session_state.second_sub_page == '1. Introducción':
-            show_intro()
-
     st.markdown(
     """
     <style>
