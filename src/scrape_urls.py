1be46ac54957970ac1c85f92d5f60a421dcce0b2
Bvallegc <116285501+Bvallegc@users.noreply.github.com>
2024-06-18 12:07:34 +0200

    Comenzamos E3, cambios en main.py scrape_urls.py y nuevo notebook e3

--- src/scrape_urls.py
+++ src/scrape_urls.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import os   
+import pandas as pd
 
 def scrape_urls(url):
     response = requests.get(url)
@@ -9,6 +11,18 @@ def scrape_urls(url):
     for link in soup.find_all('a'):
         href = link.get('href')
         if href and href.startswith('http') and href.endswith('.csv'):
+            urls.append(href)
+
+    return urls
+
+def scrape_xlsx(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    urls = []
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if href and href.startswith('http') and href.endswith('.xlsx'):
             urls.append(href)
 
     return urls
